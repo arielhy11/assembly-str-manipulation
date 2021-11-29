4f4df006629d0e2e9c434b82879972ef74cea404
arielhy11 <61493831+arielhy11@users.noreply.github.com>
2021-11-29 17:18:16 +0200

    closer

--- big_file.s
+++ big_file.s
@@ -1,9 +1,6 @@
 .section .rodata
 
 .align 8
-
-.string	"choose: %d"
-
 .JUMPTABLE:
 .quad .Lfirst # case 50 
 .quad .Ldefault # case 51: loc_def 
@@ -21,12 +18,15 @@ format_c:   .string " %c"
 format_s:   .string "%s"
 format_d:   .string "%d"
 format_space:   .string "\0"
-print_pstrlen: .string "first pstring length: %d, second: %d" 
+print_pstrlen: .string "first pstring length: %d, second: %d\n"
+print_replace_char: .string "old char: %c, new char: %c, first string %s, second string %s\n"
+
 
 .text
 
 ###### BELONGS TO run_main.s
 .global main
+.type main, @function
 main:
     movq %rsp, %rbp #for correct debugging
     # write your code here
@@ -77,29 +77,16 @@ main:
     movq -32(%rbp), %rdi
     leaq -544(%rbp), %rsi
     leaq -288(%rbp), %rdx
-    #THE CALL ITSELF to func_select 
-    
-    
-####### BELONGS TO func_select.s
-
-mov %rsi, %r12
-mov %rdx, %r13
-
-
 ##
-#HERE ARE THE TESTS
+#HERE WE JUMP
 ##
+subq $50, %rdi
+jmp *.JUMPTABLE(,%rdi,8)
 
-movq %rsi, %rdi
-movq %rdx, %rsi
+ret
 
-jmp .Lsecond
 
 #### TEST
-
-
-
-leaq -50(%rdi), %r13 #compute xi = x-50 accroding to the offset a=of the adresses. 
 
 #cmpq $10, %r13 #check if the index is in the range.
 
@@ -110,9 +97,29 @@ leaq -50(%rdi), %r13 #compute xi = x-50 accroding to the offset a=of the adresse
 #case 50   
 
 .Lfirst:
-
-movq %rsi, %rdi
+movq %rsi, %rdi #moving the strings AFTER we moved to the switch-case
 movq %rdx, %rsi
+call pstrlen
+movq %rsi, %rdi
+movq %rax, %rsi
+call pstrlen
+movq %rax, %rdx
+movq $print_pstrlen, %rdi
+xor %rax, %rax
+call printf
+xor %rax, %rax
+add $544, %rsp
+popq %rbp
+ret
+##call the func pstrlen
+
+
+	.globl	pstrlen
+	.type	pstrlen, @function
+pstrlen:
+xor %rax, %rax
+movb (%rdi), %al
+ret
 
 #case 51   
 
@@ -123,6 +130,8 @@ movq $1,  %rax
 #case 52   
 
 .Lsecond:
+movq %rsi, %rdi #moving the strings AFTER we moved to the switch-case
+movq %rdx, %rsi
 push %rbp
 movq %rsp, %rbp
 subq $24, %rsp #make room
@@ -142,34 +151,51 @@ call scanf
 
 movq %r13, %rdi #sending for the first time with first str
 movq -24(%rbp), %rsi
+movq %rsi, %r15
 movq -8(%rbp), %rdx
 #call replaceChar!!!!
-jmp .replaceChar
+call replaceChar
+movq %rax, %rcx
 
 movq %r14, %rdi #sending for the first time with second str
 movq -16(%rbp), %rsi
 movq -8(%rbp), %rdx
-#call replaceChar!!!!
+call replaceChar
+movq %rax, %r8
+
+#print:
+movq $print_replace_char, %rdi
+xor %rax, %rax
+movq %r15, %rsi
+call printf
+xor %rax, %rax
+add $24, %rsp
+pop %rbp
+add $544, %rsp
+popq %rbp
+ret
+
 
 ####FUNC replaceChar:
 
-.replaceChar:
+        .globl	replaceChar
+	.type	replaceChar, @function
+replaceChar:
+
 movq %rdi, %rax #save rdi
 addq $1, %rdi
 .for:
-    cmp $format_space, %rdi
+    cmpq $0, (%rdi)
     je .return
     
-    movq %rsi, %rdx
-    movq %rdi, %rcx
-    cmp %dl, %cl #checking if the char in rsi is equel to current char in rdi
+    cmpb %sil, %dil #checking if the char in rsi is equel to current char in rdi
     je .switch
     addq $1, %rdi
     jmp .for
 
 
 .switch:
-    movq %rdx, (%rdi)
+    movb %dl, (%rdi)
     addq $1, %rdi
     jmp .for
 .return:
@@ -177,6 +203,8 @@ addq $1, %rdi
     
 #case 53   
 .Lthird:
+movq %rsi, %rdi #moving the strings AFTER we moved to the switch-case
+movq %rdx, %rsi
 push %rbp
 movq %rsp, %rbp
 subq $24, %rsp #make room
@@ -184,12 +212,12 @@ subq $24, %rsp #make room
 movq %rdi, %r13
 movq %rsi, %r14 #have to save rsi and rdi!
 
-movq $format_d, %rdi #first char
+movq $format_d, %rdi #first num
 leaq -24(%rbp), %rsi
 xor %rax, %rax
 call scanf
 
-movq $format_d, %rdi #second char
+movq $format_d, %rdi #second num
 leaq -8(%rbp), %rsi
 xor %rax, %rax
 call scanf
@@ -204,8 +232,8 @@ movq %rdi, %rax #save rdi
 
 addq $1, %rdi #skip the str size
 addq $1, %rsi #skip the str size
-addq %rdx, (%rdi)
-addq %rdx, (%rsi)
+addq %rdx, %rdi #get to the right index
+addq %rdx, %rsi
 
 .while:
     cmp %rcx, %rdx
@@ -221,21 +249,107 @@ addq %rdx, (%rsi)
 #case 54   
 
 .Lforth:
+movq %rsi, %rdi #moving the strings AFTER we moved to the switch-case
+movq %rdx, %rsi
+jmp .swapCase #to the func
+#call Func
+movq %rsi, %rdi
+### CALL THE FUNCTION swapCase
+#the func:
+.swapCase:
+movq %rdi, %rax #save rdi
+addq $1, %rdi
+.forForth:
+    cmp $0, (%rdi)
+    je .return
+    
+    cmpb $65, (%rdi)
+    jge .secondUpperTest #if yes upCase
+    jmp .lowerCheck #if not
+    
+.secondUpperTest:
+    cmpb $90, (%rdi)
+    jle .swapDown
+    jmp .lowerCheck
+.swapDown:
+    addb $32, (%rdi)
+    addq $1, %rdi
+    jmp .forForth
 
-movq $1, %rax
+.lowerCheck:
+    cmpb $97, (%rdi)
+    jge .secondLowerCheck
+    add $1, %rdi
+    jmp .forForth
+.secondLowerCheck:
+    cmpb $122, (%rdi)
+    jle .swapUp
+.swapUp:
+    subb $32, (%rdi)
+    addq $1, %rdi
+    jmp .forForth
+    
+    
+    
+    
 
 #case 55   
 
 .Lfifth:
+movq %rsi, %rdi #moving the strings AFTER we moved to the switch-case
+movq %rdx, %rsi
+push %rbp
+movq %rsp, %rbp
+subq $24, %rsp #make room
 
-movq $1,  %rax
+movq %rdi, %r13
+movq %rsi, %r14 #have to save rsi and rdi!
+
+movq $format_d, %rdi #first num
+leaq -24(%rbp), %rsi
+xor %rax, %rax
+call scanf
+
+movq $format_d, %rdi #second num
+leaq -8(%rbp), %rsi
+xor %rax, %rax
+call scanf
+movq %r13, %rdi
+movq %r14, %rsi
+movq -24(%rbp), %rdx
+movq -8(%rbp), %rcx
+
+### CAL THE FUNC pstrijcmp
+##the func:
+movq %rdi, %rax #save rdi
+
+addq $1, %rdi #skip the str size
+addq $1, %rsi #skip the str size
+addq %rdx, %rdi #get to the right index
+addq %rdx, %rsi
+
+.whileFifth:
+    cmpq %rcx, %rdx
+    jg .return
+    movq (%rdi), %r15
+    cmpq %r15, (%rsi)
+    jg .greater
+    jl .less
+    movq (%rsi), %r10
+    movq %r10, (%rdi)
+    add $1, %rdx
+    add $1, %rdi
+    add $1, %rsi
+    jmp .whileFifth
+
+.greater:
+    movq $1, %rax
+    ret
+.less:
+    movq $-1, %rax
+
+    
+
 #NEED TO RETURN
 
 ####### BELONGS TO pstring.s
-####FUNC pstrlen:
-movzbq (%rdi), %rax
-ret
-
-
-
-ret
